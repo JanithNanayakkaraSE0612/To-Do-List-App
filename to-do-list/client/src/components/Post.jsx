354090d7c6bf7c8f3ced1e4d5927771ff9397784
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-03-22 18:00:00 +0530

    Update Post

--- to-do-list/client/src/components/Post.jsx
+++ to-do-list/client/src/components/Post.jsx
@@ -5,12 +5,42 @@ const Post = () => {
   return (
     <div className='container'>
     <div className="item_wrap">
-        <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum omnis nihil aut aperiam adipisci suscipit ullam sunt saepe cupiditate quam distinctio officiis tempore laudantium, animi amet corrupti ratione est commodi! Sunt tempora quod magnam optio, reiciendis veritatis, necessitatibus eos molestias facilis reprehenderit maiores ipsum quaerat placeat laborum, a aspernatur corporis.</div>
-        <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum omnis nihil aut aperiam adipisci suscipit ullam sunt saepe cupiditate quam distinctio officiis tempore laudantium, animi amet corrupti ratione est commodi! Sunt tempora quod magnam optio, reiciendis veritatis, necessitatibus eos molestias facilis reprehenderit maiores ipsum quaerat placeat laborum, a aspernatur corporis.</div>
+        <div className="item">
+            <h3>The First task Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam perspiciatis provident, minima aut in eum similique, impedit exercitationem perferendis tempora quibusdam cupiditate laborum earum? Libero placeat incidunt nisi ad.</p>
+            <div>
+                <div></div>
+                <div>
+                    <input type='checkbox'  />
+                     <span>Donze </span></div>
+            </div>
+        </div>
+        <div className="item"><h3>The First task Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam perspiciatis provident, minima aut in eum similique, impedit exercitationem perferendis tempora quibusdam cupiditate laborum earum? Libero placeat incidunt nisi ad.</p>
+            <div>
+                <div></div>
+                <div>
+                    <input type='checkbox'  />
+                     <span>Donze </span></div>
+            </div></div>
     </div>
     <div className="item_wrap">
-        <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum omnis nihil aut aperiam adipisci suscipit ullam sunt saepe cupiditate quam distinctio officiis tempore laudantium, animi amet corrupti ratione est commodi! Sunt tempora quod magnam optio, reiciendis veritatis, necessitatibus eos molestias facilis reprehenderit maiores ipsum quaerat placeat laborum, a aspernatur corporis.</div>
-        <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum omnis nihil aut aperiam adipisci suscipit ullam sunt saepe cupiditate quam distinctio officiis tempore laudantium, animi amet corrupti ratione est commodi! Sunt tempora quod magnam optio, reiciendis veritatis, necessitatibus eos molestias facilis reprehenderit maiores ipsum quaerat placeat laborum, a aspernatur corporis.</div>
+        <div className="item"><h3>The First task Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam perspiciatis provident, minima aut in eum similique, impedit exercitationem perferendis tempora quibusdam cupiditate laborum earum? Libero placeat incidunt nisi ad.</p>
+            <div>
+                <div></div>
+                <div>
+                    <input type='checkbox'  />
+                     <span>Donze </span></div>
+            </div></div>
+        <div className="item"><h3>The First task Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam perspiciatis provident, minima aut in eum similique, impedit exercitationem perferendis tempora quibusdam cupiditate laborum earum? Libero placeat incidunt nisi ad.</p>
+            <div>
+                <div></div>
+                <div>
+                    <input type='checkbox'  />
+                     <span>Donze </span></div>
+            </div></div>
     </div>
 </div>
   )
